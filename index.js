6b97c0c77c39bd5cbea47c3009d7bb98d16f9b1b
Ramesh Nair <ram@hiddentao.com>
2015-04-28 19:18:02 +0800

    work towards testing typeify()

--- index.js
+++ index.js
@@ -168,6 +168,152 @@ Schema.prototype._doValidate = function*(params, options) {
 }
 
 
+
+
+/**
+ * Helper to typeify()
+ */
+Schema.prototype._doTypeify = function(params) {
+  var self = this;
+
+  var schemaPath = params.schema.path, 
+    schemaNode = params.schema.node,
+    object = params.object,
+    result = params.result;
+
+
+  for (var key in schemaNode) {
+    var currentPath = schemaPath + '/' + key,
+      currentNode = schemaNode[key],
+      objectNode = object[key],
+      currentNodeType = currentNode.type;
+
+    // console.log(currentPath, currentNodeType, key, objectNode, result);
+
+    // if type not set
+    if (!currentNodeType) {
+      continue;
+    }
+
+    // missing?
+    if (undefined === objectNode || null === objectNode) {
+      continue;
+    }
+
+    switch (currentNodeType) {
+      case String:
+        if ('string' !== typeof objectNode) {
+          objectNode = '' + objectNode;
+        }
+        break;
+      case Boolean:
+        if ('boolean' !== typeof objectNode) {
+          var tmp = ('' + objectNode).toLowerCase();
+
+          if ('false' === tmp || '0' === tmp || 'no' === tmp) {
+            objectNode = false;
+          } else if ('true' === tmp || '1' === tmp || 'yes' === tmp) {
+            objectNode = true;
+          }
+        }
+        break;
+      case Number:
+        if ('number' !== typeof objectNode) {
+          tmp = '' + objectNode;
+
+          objectNode = (0 <= tmp.indexOf('.')) 
+            ? parseFloat(tmp) 
+            : parseInt(tmp);
+        }
+        break;
+      case Date:
+        if (!(objectNode instanceof Date)) {
+          objectNode = new Date(objectNode);
+        }
+        break;
+      case Object:
+      case Array:
+        // not much we can do here
+      default:
+        // if value should be an array
+        if (Array.isArray(currentNodeType)) {
+          if (Array.isArray(objectNode)) {
+            var subSchema = currentNodeType[0];
+
+            for (var index in objectNode) {
+              var item = objectNode[index];
+
+              self._doTypeify({
+                schema: {
+                  path: currentPath + '/' + index,
+                  node: subSchema,
+                },
+                object: item,
+                result: objectNode,
+              });
+            }
+          }
+        }
+        // else it just be an object
+        else {
+          self._doTypeify({
+            schema: {
+              path: currentPath,
+              node: currentNodeType,
+            },
+            object: objectNode,
+            result: objectNode,
+          });
+        }
+    }
+
+    // set final result
+    result[key] = objectNode;
+  }
+}
+
+
+
+
+/**
+ * Decode the correct type for given object's properties based on this schema.
+ *
+ * This will iterate through the object's properties. If a property path is 
+ * present in the schema then it will attempt to modify the property's value 
+ * such that its runtime type matches what the schema expects for that property.
+ *
+ * This method is useful if you have parsed JSON data which you wish to insert, 
+ * but in the case where all the property values are strings and the schema 
+ * expects some of them to be booleans, dates, etc. 
+ *
+ * Any properties in the object which are not present in the schema are left 
+ * unchanged. And any schema properties not present in the object are ignored.
+ * 
+ * @param {Object} obj Object to typeify.
+ * @return {Object} Copy of the original object with new property values.
+ */
+Schema.prototype.typeify = function(obj) {
+  if (!obj) {
+    return obj;
+  }
+
+  var newObj = {};
+
+  this._doTypeify({
+    schema: {
+      path: '',
+      node: this.schema,
+    },
+    object: obj,
+    result: newObj,
+  });
+
+  return newObj;
+}
+
+
+
+
 module.exports = function(schema) {
   return new Schema(schema);
 };
